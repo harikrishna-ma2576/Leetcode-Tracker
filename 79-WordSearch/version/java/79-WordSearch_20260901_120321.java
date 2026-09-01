59191f627b7323de37b690d8063c35272956e3b1
harikrishna-ma2576 <maharikrishna2572006@gmail.com>
2026-09-01 12:03:22 +0530

    Create 79-WordSearch

---
+++ 79-WordSearch/version/java/79-WordSearch_20260901_120321.java
@@ -0,0 +1,26 @@
+// Last updated: 01/09/2026, 12:03:21
+1class Solution {
+2    public int minPathSum(int[][] grid) {
+3        int m = grid.length;
+4        int n = grid[0].length;
+5
+6        for (int i = 1; i < m; i++) {
+7            grid[i][0] += grid[i - 1][0];
+8        }
+9
+10        for (int j = 1; j < n; j++) {
+11            grid[0][j] += grid[0][j - 1];
+12        }
+13
+14        for (int i = 1; i < m; i++) {
+15            for (int j = 1; j < n; j++) {
+16                grid[i][j] += Math.min(
+17                    grid[i - 1][j],
+18                    grid[i][j - 1]
+19                );
+20            }
+21        }
+22
+23        return grid[m - 1][n - 1];
+24    }
+25}
